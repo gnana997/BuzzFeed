f5014cce2de9c3256425f1c79daab85371d2e184
gnana997 <gnana097@gmail.com>
2017-09-13 23:23:57 +0530

    2nd edition
    just added profile pic and created login page

--- register.php
+++ register.php
@@ -1,104 +1,7 @@
 <?php  
-session_start();
-$con = mysqli_connect("localhost", "root", "", "social"); //Connection variable
-
-if(mysqli_connect_errno()) 
-{
-	echo "Failed to connect: " . mysqli_connect_errno();
-}
-
-//Declaring variables to prevent errors
-$fname = ""; //First name
-$lname = ""; //Last name
-$em = ""; //email
-$em2 = ""; //email 2
-$password = ""; //password
-$password2 = ""; //password 2
-$date = ""; //Sign up date 
-$error_array = array(); //Holds error messages
-
-if(isset($_POST['register_button'])){
-
-	//Registration form values
-
-	//First name
-	$fname = strip_tags($_POST['reg_fname']); //Remove html tags
-	$fname = str_replace(' ', '', $fname); //remove spaces
-	$fname = ucfirst(strtolower($fname)); //Uppercase first letter
-	$_SESSION['reg_fname'] = $fname; //Stores first name into session variable
-
-	//Last name
-	$lname = strip_tags($_POST['reg_lname']); //Remove html tags
-	$lname = str_replace(' ', '', $lname); //remove spaces
-	$lname = ucfirst(strtolower($lname)); //Uppercase first letter
-	$_SESSION['reg_lname'] = $lname; //Stores last name into session variable
-
-	//email
-	$em = strip_tags($_POST['reg_email']); //Remove html tags
-	$em = str_replace(' ', '', $em); //remove spaces
-	$_SESSION['reg_email'] = $em; //Stores email into session variable
-
-	//email 2
-	$em2 = strip_tags($_POST['reg_email2']); //Remove html tags
-	$em2 = str_replace(' ', '', $em2); //remove spaces
-	$_SESSION['reg_email2'] = $em2; //Stores email2 into session variable
-
-	//Password
-	$password = strip_tags($_POST['reg_password']); //Remove html tags
-	$password2 = strip_tags($_POST['reg_password2']); //Remove html tags
-
-	$date = date("Y-m-d"); //Current date
-
-	if($em == $em2) {
-		//Check if email is in valid format 
-		if(filter_var($em, FILTER_VALIDATE_EMAIL)) {
-
-			$em = filter_var($em, FILTER_VALIDATE_EMAIL);
-
-			//Check if email already exists 
-			$e_check = mysqli_query($con, "SELECT email FROM users WHERE email='$em'");
-
-			//Count the number of rows returned
-			$num_rows = mysqli_num_rows($e_check);
-
-			if($num_rows > 0) {
-				array_push($error_array, "Email already in use<br>");
-			}
-
-		}
-		else {
-			array_push($error_array, "Invalid email format<br>");
-		}
-
-
-	}
-	else {
-		array_push($error_array, "Emails don't match<br>");
-	}
-
-
-	if(strlen($fname) > 25 || strlen($fname) < 2) {
-		array_push($error_array, "Your first name must be between 2 and 25 characters<br>");
-	}
-
-	if(strlen($lname) > 25 || strlen($lname) < 2) {
-		array_push($error_array,  "Your last name must be between 2 and 25 characters<br>");
-	}
-
-	if($password != $password2) {
-		array_push($error_array,  "Your passwords do not match<br>");
-	}
-	else {
-		if(preg_match('/[^A-Za-z0-9]/', $password)) {
-			array_push($error_array, "Your password can only contain english characters or numbers<br>");
-		}
-	}
-
-	if(strlen($password > 30 || strlen($password) < 5)) {
-		array_push($error_array, "Your password must be betwen 5 and 30 characters<br>");
-	}
-}
-
+require 'config/config.php';
+require 'include/form_handlers/register_handler.php';
+require 'include/form_handlers/login_handler.php';
 ?>
 
 <html>
@@ -106,6 +9,23 @@ if(isset($_POST['register_button'])){
 	<title>Welcome to BuzzFeed!</title>
 </head>
 <body>
+
+	<form action="register.php" method="POST">
+		<input type="email" name="log_email" placeholder = "Enter your Email" value="<?php 
+		if(isset($_SESSION['log_email'])) {
+			echo $_SESSION['log_email'];
+		} 
+		?>" required>
+		<br>
+		<input type="password" name="log_password" placeholder = "Password" required>
+		<br>
+		<input type="submit" name="login_button" value="Login">
+
+		<?php if(in_array("Email or password was incorrect<br>",$error_array)) echo "Email or password was incorrect<br>";  ?>
+
+		<br>
+	</form>
+
 
 	<form action="register.php" method="POST">
 		<input type="text" name="reg_fname" placeholder="First Name" value="<?php 
@@ -156,6 +76,7 @@ if(isset($_POST['register_button'])){
 
 		<input type="submit" name="register_button" value="Register">
 		<br>
+		<?php if(in_array("<p style='color:#14C800;'><b>You're registered successfully!! Now Goahead and Login!!</b></p><br>", $error_array)) echo "<p style='color:#14C800;'><b>You're registered successfully!! Now Goahead and Login!!</b></p><br>"; ?> 
 
 	</form>
 
